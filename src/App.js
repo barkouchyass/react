e1faf25f84ff4d92584a31a89ad9243bef74972c
barkouchyass <barkouchyass@gmail.com>
2025-04-04 15:26:36 +0000

    about to go over props

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 
 import './App.css';
+import Employee from './components/Employee';
 
 function App() {
+  const showEmployees = true ;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          hello there!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showEmployees ?(
+      <>
+       <Employee/>
+       <Employee/>
+       <Employee/>
+       <Employee/>
+       <Employee/>
+       </>)
+       :
+       (<p>you cannot see the employees</p>) 
+       }
     </div>
   );
 }
