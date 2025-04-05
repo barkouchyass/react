547da443c730e1684cc0db0d6cdef11e0cfb6c30
barkouchyass <barkouchyass@gmail.com>
2025-04-05 13:47:26 +0000

    create function edit Employee

--- src/App.js
+++ src/App.js
@@ -1,69 +1,88 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState(
-    [ 
+    [
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
+        id: 1,
+        name: "Alexandre",
+        role: "Designer",
+        img: "https://images.pexels.com/photos/2169434/pexels-photo-2169434.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+        id: 2,
+        name: "Sophie",
+        role: "Project Manager",
+        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
+        id: 3,
+        name: "Julien",
+        role: "Data Analyst",
+        img: "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg"
+        id: 4,
+        name: "Clara",
+        role: "Product Manager",
+        img: "https://images.pexels.com/photos/773371/pexels-photo-773371.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg"
+        id: 5,
+        name: "Luca",
+        role: "Marketing Specialist",
+        img: "https://images.pexels.com/photos/2083751/pexels-photo-2083751.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg"
+        id: 6,
+        name: "Emma",
+        role: "Content Writer",
+        img: "https://images.pexels.com/photos/2128807/pexels-photo-2128807.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2380795/pexels-photo-2380795.jpeg"
+        id: 7,
+        name: "Daniel",
+        role: "UX/UI Designer",
+        img: "https://images.pexels.com/photos/2380795/pexels-photo-2380795.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
+        id: 8,
+        name: "Olivia",
+        role: "Software Engineer",
+        img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg"
+        id: 9,
+        name: "Benjamin",
+        role: "QA Tester",
+        img: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/2097475/pexels-photo-2097475.jpeg"
+        id: 10,
+        name: "Maya",
+        role: "Customer Support",
+        img: "https://images.pexels.com/photos/2097475/pexels-photo-2097475.jpeg"
       },
       {
-        name : "yassine", 
-        role : "Developper", 
-        img :"https://images.pexels.com/photos/1642161/pexels-photo-1642161.jpeg"
+        id: 11,
+        name: "Zoe",
+        role: "HR Manager",
+        img: "https://images.pexels.com/photos/1642161/pexels-photo-1642161.jpeg"
       }
-
     ]
-  ); 
+  );
+  function updateEmployee(id, newName, newRole) {
+    const updateEmployees = employees.map((employee)=>{
+      if(id == employee.id){
+        return{...employee, name:newName, role:newRole};
+      }
+      return employee;
+    });
+    setEmployees(updateEmployees);
+  }
   const showEmployees = true;
   return (
     <div className="App">
@@ -74,17 +93,18 @@ function App() {
             <input
               type="text"
               onChange={(e) => {
-                console.log(e.target.value);
                 setRole(e.target.value);
               }} />
             <div className="flex flex-wrap justify-center">
               {employees.map((employee) => {
-                return(
-                <Employee 
-                  key = {uuidv4()}
-                  name = {employee.name}
-                  role = {employee.role}
-                  img = {employee.img}
+                return (
+                  <Employee
+                    key={employee.id}
+                    id={employee.id}
+                    name={employee.name}
+                    role={employee.role}
+                    img={employee.img}
+                    updateEmployee={updateEmployee}
                   />
                 );
               })}
