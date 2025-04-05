126d18c56ca2a4474b7139a286fb6a6a570b2b36
barkouchyass <barkouchyass@gmail.com>
2025-04-05 14:26:41 +0000

    create addEmployee function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import AddEmployee from './components/AddEmployee';
 function App() {
   const [role, setRole] = useState('dev');
   const [employees, setEmployees] = useState(
@@ -83,6 +84,15 @@ function App() {
     });
     setEmployees(updateEmployees);
   }
+  function newEmployee(name, role, img) {
+    const newEmployee={
+      id:uuidv4(),
+      name:name,
+      role:role,
+      img:img
+    }
+    setEmployees([...employees, newEmployee])
+  }
   const showEmployees = true;
   return (
     <div className="App">
@@ -109,6 +119,7 @@ function App() {
                 );
               })}
             </div>
+            <AddEmployee newEmployee={newEmployee}/>
           </>
         )
         :
