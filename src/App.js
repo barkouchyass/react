5397c8441aae10db9e8e5d427e91e5e89ce254dd
barkouchyass <barkouchyass@gmail.com>
2025-04-08 18:37:57 +0100

    add the routes

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import { v4 as uuidv4 } from 'uuid';
 import AddEmployee from './components/AddEmployee';
 import Header from './components/Header';
 import Employees from './pages/Employees';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import Customers from './pages/Customers';
 function App() {
  
   return (
     <Header>
-  <Employees/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/employees' element={<Employees/>} />
+          <Route path='/customers' element={<Customers/>} />
+        </Routes>
+      </BrowserRouter>
   </Header>
 
 )
