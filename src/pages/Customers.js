49c7bb4279be86675e5685470fd0d8a4a27189e0
barkouchyass <barkouchyass@gmail.com>
2025-04-12 21:47:51 +0100

    create customer.js

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,26 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 export default function Customers() {
-    return <h1>hello Customers</h1>;
+    const [customers, setCustomers] = useState();
+    useEffect(() => {
+        console.log('Fetching...');
+        fetch('http://127.0.0.1:8000/api/customers')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setCustomers(data.customers);
+            });
+    }, []);
+    return (
+        <>
+            <h1>here are our customers :</h1>
+            {customers ? customers.map((customer) => {
+                return (
+                    <p>
+                        <Link to={"/customers/"+customer.id} key={customer.id}>{customer.name}</Link>
+                    </p>
+                )
+            }) : null}
+        </>
+    );
 }
